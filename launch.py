e665dfdfc64fa1f58fdde55852b9ae636b10f62e
hben35096 <hben35096@gmail.com>
2025-07-22 22:49:04 +0800

    Add UI test mode

--- launch.py
+++ launch.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description="GFlowUI 启动脚本", add_help=Tr
 parser.add_argument('--back-port', nargs='?', const=8188, default=8188, type=int, help='指定后端服务端口号，不指定则为8188')
 parser.add_argument('--port', nargs='?', const=7860, default=7860, type=int, help='指定前端服务端口号，不指定则为7860') # default 完全不提供 --port 的情况下
 parser.add_argument('--dl-way', nargs='?', const='ms', default='ms', help='指定缺失模型下载方式，如 ms 或 cg')
+parser.add_argument('--ui-test', action='store_true', help="允许在不启动后端的模式下启动，以测试部分UI功能")
 args = parser.parse_args()
 
 self_dir = os.path.abspath(os.path.dirname(__file__))
@@ -54,6 +55,12 @@ def launch_app(command, back_app_path=None):
     except Exception as e:
         print(f"❌ 发生错误: {e}")
 
-
-command = command = ["python", "main.py"] + [f"--port={int(args.back_port)}"]
-launch_app(command, back_app_path)
+if __name__ == "__main__":
+    if args.ui_test:
+        app_url = "http://127.0.0.1:8188"
+        from frontend import common as grui
+        demo = grui.gradio_ui(app_url, back_app_path, args.dl_way)
+        demo.launch(server_port=int(args.port),)
+    else:
+        command = command = ["python", "main.py"] + [f"--port={int(args.back_port)}"]
+        launch_app(command, back_app_path)
